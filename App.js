c0f3571e37f0430efe2cfb73d6da99455ab2ed94
Hector <stanleychike@yahoo.com>
2022-01-26 15:20:58 +0100

    created empApp with createuser, userlist and userdetail screens

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { CreateStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import UserList from './screens/UserList';
+import UserDetailScreen from './screens/UserDetailScreen';
+import CreateUserScreen from './screens/CreateUserScreen';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return ( 
+    <Stack.Navigator>
+      <Stack.Screen name="UserList" component={UserList} />
+      <Stack.Screen name="UserDetailScreen" component={UserDetailScreen} />
+      <Stack.Screen name="CreateUserScreen" component={CreateUserScreen} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <MyStack/>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/UserDetailScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserDetailScreen = () => {
+    return (
+        <div>
+
+        </div>)
+};
+
+export default UserDetailScreen;
